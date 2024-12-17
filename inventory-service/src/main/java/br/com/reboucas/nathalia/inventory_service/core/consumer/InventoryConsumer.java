d2a7b38bf89ae11d3efb7c46cb458f52397676df
Nathália Rebouças <nathreboucas5@gmail.com>
2024-12-16 21:29:04 -0300

    criando os consumers

---
+++ inventory-service/src/main/java/br/com/reboucas/nathalia/inventory_service/core/consumer/InventoryConsumer.java
@@ -0,0 +1,34 @@
+package br.com.reboucas.nathalia.inventory_service.core.consumer;
+
+import br.com.reboucas.nathalia.inventory_service.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class InventoryConsumer {
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.inventory-success}"
+    )
+    public void consumeSuccessEvent(String payload) {
+        log.info("Receiving success event {} from inventory-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.inventory-fail}"
+    )
+    public void consumeFailEvent(String payload) {
+        log.info("Receiving rollback event {} from inventory-fail topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+}
---
+++ order-service/src/main/java/br/com/reboucas/nathalia/order_service/core/consumer/EventConsumer.java
@@ -0,0 +1,24 @@
+package br.com.reboucas.nathalia.order_service.core.consumer;
+
+import br.com.reboucas.nathalia.order_service.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class EventConsumer {
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.notify-ending}"
+    )
+    public void consumeNotifyEndingEvent(String payload) {
+        log.info("Receiving ending notification event {} from notify-ending topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+}
