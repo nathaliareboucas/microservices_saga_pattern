b86923a0888c4cba96e652806e42336c656f95c8
Nathália Rebouças <nathreboucas5@gmail.com>
2024-12-17 07:25:04 -0300

    lógica de recebimento de notificação de finalização saga

--- order-service/src/main/java/br/com/reboucas/nathalia/order_service/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/reboucas/nathalia/order_service/core/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package br.com.reboucas.nathalia.order_service.core.consumer;
 
+import br.com.reboucas.nathalia.order_service.core.service.EventService;
 import br.com.reboucas.nathalia.order_service.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class EventConsumer {
     private final JsonUtil jsonUtil;
+    private final EventService eventService;
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
@@ -19,6 +21,6 @@ public class EventConsumer {
     public void consumeNotifyEndingEvent(String payload) {
         log.info("Receiving ending notification event {} from notify-ending topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        eventService.notifyEnding(event);
     }
 }
--- order-service/src/main/java/br/com/reboucas/nathalia/order_service/core/service/EventService.java
+++ order-service/src/main/java/br/com/reboucas/nathalia/order_service/core/service/EventService.java
@@ -4,12 +4,14 @@ import br.com.reboucas.nathalia.order_service.core.document.Event;
 import br.com.reboucas.nathalia.order_service.core.document.Order;
 import br.com.reboucas.nathalia.order_service.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EventService {
     private final EventRepository repository;
 
@@ -26,5 +28,11 @@ public class EventService {
                 .build();
         save(event);
         return event;
+    }
+
+    public void notifyEnding(Event event) {
+        event.setCreatedAt(LocalDateTime.now());
+        save(event);
+        log.info("Order {} with saga notified! transactionId: {}", event.getOrderId(), event.getTransactionId());
     }
 }
