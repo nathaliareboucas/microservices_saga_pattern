ea28cca2b6f6d743a5d1f962f00cfc134c1c2727
Nathália Rebouças <nathreboucas5@gmail.com>
2024-12-17 15:34:38 -0300

    criando entidades do product-validarion-service

---
+++ product-validation-service/src/main/java/br/com/reboucas/nathalia/product_validation_service/core/model/Product.java
@@ -0,0 +1,21 @@
+package br.com.reboucas.nathalia.product_validation_service.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String code;
+}
